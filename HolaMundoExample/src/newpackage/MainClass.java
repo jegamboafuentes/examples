034e911c3446322ec4fc15eda71c934f4768f822
Jorge Enrique Gamboa Fuentes <jegamboafuentes@gmail.com>
2015-07-11 16:27:50 -0500

    Se agrego el primer hola mundo

--- HolaMundoExample/src/newpackage/MainClass.java
+++ HolaMundoExample/src/newpackage/MainClass.java
@@ -5,6 +5,6 @@ package newpackage;
 //This is a Hello World Example
 public class MainClass {
     public static void main(String[] args) {
-        
+        System.out.println("Hola GIT World");
     }
 }
